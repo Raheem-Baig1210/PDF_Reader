99c4aa81624e8da0144928d2e4abe572ff0bafba
Raheem-Baig1210 <raheembaig822@gmail.com>
2025-06-13 00:13:03 +0530

    Uppdated

--- App.jsx
+++ App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <Image 
       style={{width: 200 , height: 300}}
       source={{uri:"https://images.unsplash.com/photo-1747102330549-66897cb65d2c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}}/>
-      <TouchableOpacity style={{padding:10, backgroundColor: "blue"}} onPress={()=> Alert.alert("Button Pressed")}>
+      <TouchableOpacity style={{padding:10, backgroundColor: "green"}} onPress={()=> Alert.alert("Button Pressed....!!!!")}>
       <Text>Button</Text>
     </TouchableOpacity>
     </View>
